640231881cb6ae99e625ffd333afd276e9fb5a21
PG Losembe <pglosembe@gmail.com>
2026-09-04 21:27:16 -0400

    Frozen settings

--- Data/Scripts/998_ZZZ_Extensions/001_Settings.rb
+++ Data/Scripts/998_ZZZ_Extensions/001_Settings.rb
@@ -1,7 +1,20 @@
 module Settings
+  # Keep the local DownloadedSettings.rb. Set false to allow upstream updates.
+  FREEZE_REMOTE_SETTINGS = true
+
   # The odds of a newly generated Pokémon being shiny (out of 65536).
   SHINY_POKEMON_CHANCE = 3277 #(MECHANICS_GENERATION >= 6) ? 16 : 8
   STARTUP_MESSAGES = "ZZZ_SETTINGS LOADED."
   # FISHING_AUTO_HOOK = true  
 end
 
+module FrozenRemoteSettings
+  private
+
+  def updateHttpSettingsFile
+    return if Settings::FREEZE_REMOTE_SETTINGS
+    super
+  end
+end
+
+Object.prepend(FrozenRemoteSettings)
